25ec5dd3809023fb3fd5e6b5e70c2efb703de33b
David Wisner <dwisner6@gmail.com>
2015-05-02 22:18:08 -0700

    first version

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 var postcss = require('postcss');
-
+var styles = {};
 module.exports = postcss.plugin('postcss-component', function (opts) {
   opts = opts || {};
 
     // Work with options here
 
-  return function (css) {
+  return function (css, result) {
     css.eachAtRule('component', function (component) {
       component.eachRule(function(rule) {
-        var selector = rule.selector;
-        if (selector.startsWith('.')) {
-          selector = selector.slice(1);
-        }
-        rule.selector = selector+':';
-        // rule.replaceValues(/\d+%/, {}, function(string) {
-        //   return '"'+string+'"'
-        // });
-        // console.log(rule.toString().replace(/\;/g, ','));
-        var styles = {};
+        var compStyle = styles[component.params] = {};
         rule.nodes.forEach(function(val) {
           if (val.type === 'decl') {
-            styles[val.prop] = val.value;
+            compStyle[val.prop] = val.value;
           }
         })
-        console.log(styles);
       })
-
-      // console.log(component.nodes)
       component.removeSelf();
     });
+    result.styles = styles;
   };
 });
--- test/test.js
+++ test/test.js
@@ -6,6 +6,10 @@ var plugin = require('../');
 var test = function (input, output, opts, done) {
     postcss([ plugin(opts) ]).process(input).then(function (result) {
         expect(result.css).to.eql(output);
+        expect(result.styles).to.eql({
+          App: { width: '100%', height: '200%' },
+          Potato: { background: 'blue', position: 'relative' }
+        });
         done();
     });
 };
@@ -13,7 +17,7 @@ var test = function (input, output, opts, done) {
 describe('postcss-component', function () {
 
     it('does something', function (done) {
-        test('@component App {.main {width: 100%;height: 200%;}}.main{width: 100%}', '.main{width: 100%}', {}, done);
+        test('@component App {.main {width: 100%;height: 200%;}}.main{width: 100%}@component Potato {.main {background: blue;position: relative;}}', '.main{width: 100%}', {}, done);
     });
 
 });
